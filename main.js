51de6ed3a8438d56fbf4c6a891088a61ad46b234
Leotagliaferro <tagliaferro.leonardo@gmail.com>
2023-04-19 15:31:07 -0400

    :construction: create-boardgame

--- main.js
+++ main.js
@@ -1,12 +1,9 @@
 import './src/styles/settings/colors.css';
 import './src/styles/generic/reset.css'
 import "./src/styles/elements/base.css";
+import BoardGame from './src/objects/BoardGame';
 
-import CardGame from "./src/components/CardGame";
 
 const $root = document.querySelector('#root');
-const $htmlCardGame = CardGame();
-
-console.log($htmlCardGame);
-
-$root.insertAdjacentHTML("beforeend", $htmlCardGame);
+const $htmlBoardGame = BoardGame(6);
+$root.insertAdjacentHTML("beforeend", $htmlBoardGame);
