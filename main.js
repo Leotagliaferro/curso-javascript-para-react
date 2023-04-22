39ec9d66df8dd1114692a5b099bf2dfb07b1ad31
Leotagliaferro <tagliaferro.leonardo@gmail.com>
2023-04-22 17:29:28 -0400

    :sparkles: Um novo objeject adicionado

--- main.js
+++ main.js
@@ -2,7 +2,8 @@ import './src/styles/settings/colors.css';
 import './src/styles/generic/reset.css'
 import "./src/styles/elements/base.css";
 import BoardGame from './src/objects/BoardGame';
-import PlayerName from './src/components/PlayerName';
+import ScoreBoard from './src/objects/scoreboard';
+
 
 
 const $root = document.querySelector('#root');
@@ -10,8 +11,7 @@ const $root = document.querySelector('#root');
 $root.insertAdjacentHTML(
     "beforeend", 
     `
-    ${PlayerName("Player1")}
-    ${PlayerName("Player2")}
+    ${ScoreBoard()}
     ${BoardGame(6)}
     `
     
